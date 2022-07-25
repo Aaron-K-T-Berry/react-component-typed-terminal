21b4f345b331c9fdd4ebef038e065e3becfbe14e
Aaron-K-T-Berry <aaron.abm@gmail.com>
2022-07-25 21:14:11 +1000

    adding publish step

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/*"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
